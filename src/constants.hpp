e24c529dfb54b8df6bf9d8824731aa3ed13d0482
Aditya Shridhar Hegde <adishegde@gmail.com>
2019-04-29 12:08:07 +0530

    Update length of prime number

--- src/constants.hpp
+++ src/constants.hpp
@@ -2,9 +2,9 @@
 #define _CONSTANTS_HPP
 
 #define uchar unsigned char
-#define P_LEN 8       // length of prime number in bits
+#define P_LEN 64       // length of prime number in bits
 #define SECRET_LEN 16   // length of secret in bytes
-#define STORAGE_LEN 2 // length of uchar array for storing ZZ and ZZ_p data in bytes
+#define STORAGE_LEN 10 // length of uchar array for storing ZZ and ZZ_p data in bytes
 
 #define INIT_FILE "./gen_out.dat"
 
